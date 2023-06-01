ed806637dfd7755407f4d2de49b3f0bdf9f083a0
David Krismer <david.krismer71@gmail.com>
2023-05-31 17:30:36 -0700

    Added: remove option to character select,  all form fields to character select, added DEFAULT_JSON file and constant variable. Fixed: input field not re-rendering with relevant character name/information

--- src/react-modules/TextInput.js
+++ src/react-modules/TextInput.js
@@ -1,31 +1,58 @@
 import {useState, useEffect} from 'react';
 
-function TextInput({ label, personaIndex, element, handleJSONUpdate, showcaseJSON, selectedCharacter}) {
+function TextInput({ label, personaIndex, element, handleJSONUpdate, showcaseJSON}) {
 
   const [value, setValue] = useState("");
 
   useEffect(() => {
-    setValue(showcaseJSON.personas[personaIndex].element)
-  }, [selectedCharacter]);
+    setValue(showcaseJSON.personas[personaIndex][element])
+  }, [personaIndex]);
 
 
-    const handleChange = (event) => {
-        setValue(event.target.value) 
-        handleJSONUpdate(personaIndex, element, event.target.value);
-        console.log(showcaseJSON.personas[personaIndex][element])
+    const handleChange = (newValue) => {
+        setValue(newValue) 
+        handleJSONUpdate(personaIndex, element, newValue);
       };
 
 
   return (
-    <div className="p-2">
-    <label className="text-neutral-500 dark:text-neutral-200" for={`${personaIndex}_${element}`}>{label} </label>
-    <input className="p-1" id={`${personaIndex}_${element}`} type="text" 
-    value={showcaseJSON.personas[personaIndex].element}
-    defaultValue={showcaseJSON.personas[personaIndex].element}
-    onChange={handleChange} />
+    <div className="p-1">
+      <label className="text-neutral-500 dark:text-neutral-200" for={`${personaIndex}_${element}`}>{label} </label>
+      <br/>
+      <input className="p-1 w-full" id={`${personaIndex}_${element}`} type="text" 
+      value={value}
+      onChange={(e) => handleChange(e.target.value)} />
     </div>
     
   );
 }
 
-export { TextInput };
+
+function TextAreaInput({ label, personaIndex, element, handleJSONUpdate, showcaseJSON}) {
+
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    setValue(showcaseJSON.personas[personaIndex][element])
+  }, [personaIndex]);
+
+
+    const handleChange = (newValue) => {
+        setValue(newValue) 
+        handleJSONUpdate(personaIndex, element, newValue);
+      };
+
+
+  return (
+    <div className="p-1 w-full">
+      <label className="text-neutral-500 dark:text-neutral-200" for={`${personaIndex}_${element}`}>{label} </label>
+      <br/>
+      <textarea className="p-1 w-full resize-none" id={`${personaIndex}_${element}`} type="text" 
+      value={value}
+      onChange={(e) => handleChange(e.target.value)} />
+    </div>
+    
+  );
+}
+
+export { TextInput, TextAreaInput };
--- src/react-modules/character-screen/CharacterScreen.js
+++ src/react-modules/character-screen/CharacterScreen.js
@@ -1,51 +1,124 @@
-import {NewCharacterButton} from './NewCharacterButton'
-import {TextInput} from './../TextInput'
+import { NewCharacterButton } from "./NewCharacterButton";
+import { TextInput, TextAreaInput } from "./../TextInput";
+import { FileUploadFull } from "./../FileUpload";
 
-function CharacterScreen({ showcaseJSON, setShowcaseJSON, selectedCharacter, setSelectedCharacter, handleJSONUpdate}) {
-
+function CharacterScreen({
+  showcaseJSON,
+  setShowcaseJSON,
+  selectedCharacter,
+  setSelectedCharacter,
+  handleJSONUpdate,
+}) {
   const handleClick = (e) => {
-    setSelectedCharacter(e.target.value)
-  }
+    setSelectedCharacter(e.target.value);
+  };
 
+  const handleRemove = (e, i) => {
+    if (showcaseJSON.personas.length == 1) return;
 
+    // Prevent out of bounds selected character
+    if (
+      (selectedCharacter == i ||
+        showcaseJSON.personas.length - 1 == selectedCharacter) &&
+      selectedCharacter != 0
+    ) {
+      setSelectedCharacter(selectedCharacter - 1);
+    }
+
+    setShowcaseJSON((json) => {
+      json["personas"].splice(i, 1);
+    });
+  };
 
   return (
     <div className="border grid grid-cols-2">
       <div className="flex justify-center items-center flex-col">
-
         <h2 className="text-1xl text-neutral-700 dark:text-white font-bold mb-5">
-          SELECT CHARACTER
+          Select Your Character
         </h2>
+        <p>Select a character below or create a new one.</p>
 
-        <div className="grid grid-cols-4">
+        <div className="grid grid-cols-3">
           {showcaseJSON.personas.map((person, index) => (
-            <div key={index} className={`border p-1 m-3 flex justify-center items-center flex-col ${selectedCharacter == index ? 'bg-white' : ''}`} >
-              <p className="text-center">{person.name}</p>
-              <button className="bg-white border rounded p-1 m-3"
+            <div
+              key={index}
+              className={`border p-1 m-3 flex justify-center items-center flex-col ${
+                selectedCharacter == index ? "bg-white" : ""
+              }`}
+            >
+              {/* REMOVE BUTTON */}
+              <div className="ml-auto">
+                {
+                  // Conditionally render the remove button. There must always be one character.
+                  showcaseJSON.personas.length > 1 ? (
+                    <button
+                      className="border p-1 bg-red-400 rounded"
+                      onClick={(event) => handleRemove(event, index)}
+                    >
+                      [x]
+                    </button>
+                  ) : null
+                }
+              </div>
+
+              <div className="p-2">
+                <p className="text-center">{person.name}</p>
+              </div>
+
+              {/* SELECT BUTTON */}
+              <button
+                className="bg-white border rounded p-1 m-3"
                 value={index}
-                onClick={handleClick}>
-                Select {person.name}
+                onClick={handleClick}
+              >
+                Select
               </button>
             </div>
           ))}
         </div>
 
-        <NewCharacterButton showcaseJSON={showcaseJSON} setShowcaseJSON={setShowcaseJSON} setSelectedCharacter={setSelectedCharacter}/>
-
+        {/* ADD BUTTON */}
+        <div className="ml-auto">
+          <NewCharacterButton
+            showcaseJSON={showcaseJSON}
+            setShowcaseJSON={setShowcaseJSON}
+            setSelectedCharacter={setSelectedCharacter}
+          />
+        </div>
       </div>
 
-      <div className="flex justify-center items-center flex-col">
+      <div className="p-3 flex justify-center items-center flex-col">
         <h2 className="text-1xl text-neutral-700 dark:text-white font-bold mb-5">
           EDIT CHARACTER
         </h2>
-        <TextInput 
+        <div className="grid grid-cols-2 w-full">
+          <TextInput
             label={"Name:"}
             personaIndex={selectedCharacter}
             element={"name"}
             handleJSONUpdate={handleJSONUpdate}
             showcaseJSON={showcaseJSON}
-            selectedCharacter={selectedCharacter}
           />
+          <TextInput
+            label={"Role:"}
+            personaIndex={selectedCharacter}
+            element={"type"}
+            handleJSONUpdate={handleJSONUpdate}
+            showcaseJSON={showcaseJSON}
+          />
+        </div>
+        <TextAreaInput
+          label={"Page Description:"}
+          personaIndex={selectedCharacter}
+          element={"description"}
+          handleJSONUpdate={handleJSONUpdate}
+          showcaseJSON={showcaseJSON}
+        />
+        <div className="grid grid-cols-3 w-full">
+          <FileUploadFull text={"Body Image"} />
+          <FileUploadFull text={"Avatar Image"} />
+          <FileUploadFull text={"Celebration Image"} />
+        </div>
       </div>
     </div>
   );

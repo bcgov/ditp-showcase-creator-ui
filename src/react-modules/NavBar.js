d17d010a98aa98a563080d156df6b8e00de0646e
David Abram-Krismer <david.krismer71@gmail.com>
2023-06-01 00:11:35 -0700

    Json upload module (#13)
    * Added a feature to load json files, also an error modal
    
    * refactored nav bar, added style for load button
    
    * formatting update

--- src/react-modules/NavBar.js
+++ src/react-modules/NavBar.js
@@ -1,22 +1,24 @@
 import { DarkModeToggle } from "./DarkModeToggle";
 import { SaveButton } from "./SaveButton";
 import { SaveModal } from "./SaveModal";
+import {JSONUploadButton} from './JSONUpload'
 import { useState } from "react";
 
-function NavBar({ darkMode, setDarkMode, showcaseJSON }) {
+function NavBar({ darkMode, setDarkMode, showcaseJSON, setShowcaseJSON }) {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <div className="grid grid-cols-3">
-      <div />
+      <div className="flex justify-center items-center"><DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} /></div>
       <h1 className="text-4xl text-center p-10 dark:text-white">
         Showcase Creator UI
       </h1>
       <div className="flex justify-center items-center">
-        <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
+        
         {showModal ? (
           <SaveModal setShowModal={setShowModal} showcaseJSON={showcaseJSON} />
         ) : null}
+        <JSONUploadButton setShowcaseJSON={setShowcaseJSON} />
         <SaveButton setShowModal={setShowModal} />
       </div>
     </div>

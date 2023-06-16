d706e1a25f94b2d162319e11a16b8e8a3474301d
boadoryan <75857938+boadoryan@users.noreply.github.com>
2023-06-16 11:50:24 -0700

    .

--- src/react-modules/credentials/components/Form.js
+++ src/react-modules/credentials/components/Form.js
@@ -68,45 +68,45 @@ function Form({
 }
 
 export { Form };
-import React from "react";
-import { TextInput } from "../../TextInput";
-import { LocalTextInput } from "../../LocalTextInput";
-import { FormHeader } from "./FormHeader";
-import { NewCredentialButton } from "../NewCredentialButton";
-import { AttributesList } from "../AttributesList";
+// import React from "react";
+// import { TextInput } from "../../TextInput";
+// import { LocalTextInput } from "../../LocalTextInput";
+// import { FormHeader } from "./FormHeader";
+// import { NewCredentialButton } from "../NewCredentialButton";
+// import { AttributesList } from "../AttributesList";
 
-function Form({
-  selectedIndex,
-  credentialName,
-  issuerName,
-  showcaseJSON,
-  handleJSONUpdate,
-  setShowcaseJSON,
-  localJSON,
-  handleLocalUpdate,
-  saveJSON,
-}) {
-  return (
-    <>
-      <FormHeader
-        formTitle={"Create your Credential"}
-        formCategory={"Credential"}
-        setShowcaseJSON={setShowcaseJSON}
-      ></FormHeader>
-      <LocalTextInput
-        label="Credential Name"
-        personaIndex={selectedIndex}
-        element={["onboarding", 4, "credentials"]}
-        handleJSONUpdate={handleLocalUpdate}
-        showcaseJSON={showcaseJSON}
-        localJSON={localJSON}
-      />
-      <button onClick={saveJSON}>save</button>
-      <NewCredentialButton
-        setShowcaseJSON={setShowcaseJSON}
-      ></NewCredentialButton>
-    </>
-  );
-}
+// function Form({
+//   selectedIndex,
+//   credentialName,
+//   issuerName,
+//   showcaseJSON,
+//   handleJSONUpdate,
+//   setShowcaseJSON,
+//   localJSON,
+//   handleLocalUpdate,
+//   saveJSON,
+// }) {
+//   return (
+//     <>
+//       <FormHeader
+//         formTitle={"Create your Credential"}
+//         formCategory={"Credential"}
+//         setShowcaseJSON={setShowcaseJSON}
+//       ></FormHeader>
+//       <LocalTextInput
+//         label="Credential Name"
+//         personaIndex={selectedIndex}
+//         element={["onboarding", 4, "credentials"]}
+//         handleJSONUpdate={handleLocalUpdate}
+//         showcaseJSON={showcaseJSON}
+//         localJSON={localJSON}
+//       />
+//       <button onClick={saveJSON}>save</button>
+//       <NewCredentialButton
+//         setShowcaseJSON={setShowcaseJSON}
+//       ></NewCredentialButton>
+//     </>
+//   );
+// }
 
-export { Form };
+// export { Form };

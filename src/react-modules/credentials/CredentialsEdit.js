c4dfd95a8915e0185b6025abc7993cfe62c07741
boadoryan <75857938+boadoryan@users.noreply.github.com>
2023-07-13 19:34:50 -0700

    Testing new cred page (#36)
    * Attributes can be added dynamically
    
    * .
    
    * first commit
    
    * inputs change the data, attribues and credentials can be added.
    
    * changing things to new inputs
    
    * .
    
    * redid cred page
    
    * .
    
    * recent work.
    
    * fixed editing and adding credentials.
    
    * updated credential component. removed edit, moved delete functionality.
    
    * fixed styling and added select logic
    
    * fixed all bugs. ready for testing
    
    * .
    
    * fixed reset json button.
    
    * small change
    
    * removed extra files from testing new components.
    
    * cleaned up all files, renamed files to appropriate names.
    
    * lifted state up from CredentialsScreen into App. Credential JSON is now accessible through whole app.
    
    * removed extra
    
    * deleted duplicate
    
    * fixed import changes after changing file names.
    
    * first commit on new branch.
    
    * added proof request component
    
    * .
    
    * .
    
    * testing new stuff with nesting.
    
    * testing
    
    * .
    
    * .
    
    * made a few changes to work with the updated json format.
    
    * bug fixing
    
    * removed extra file
    
    * Updated to use default json, some changes to functionality--nothing is updated until the user presses add or done
    
    * fixed all known bugs and added attribute functionality.
    
    * removed SetupPage as its been renamed.
    
    * removed console.logs
    
    ---------
    
    Co-authored-by: David Krismer <david.krismer71@gmail.com>

--- src/react-modules/credentials/CredentialsEdit.js
+++ src/react-modules/credentials/CredentialsEdit.js
@@ -1,27 +1,13 @@
-import React from "react";
-import { CredentialAttributesList } from "./components/AttributesList";
+import { CredentialAttributesList } from "./components/CredentialAttributesList";
 
 function CredentialsEdit({
   selectedCredential,
   tempData,
-  handleChange,
-  addAttribute,
   setTempData,
+  addAttribute,
+  handleChange,
+  removeAttribute,
 }) {
-  const handleAttributeRemoval = (attributeIndex) => {
-    setTempData((prevData) => {
-      const newData = [...prevData];
-      const selectedCred = { ...newData[selectedCredential] }; // Create a copy of the selected credential
-      selectedCred.attributes = selectedCred.attributes.filter(
-        (_, index) => index !== attributeIndex
-      );
-      newData[selectedCredential] = selectedCred; // Update the selected credential in the new data array
-      return newData;
-    });
-  };
-
-  if (tempData.length === 0) return;
-
   return (
     <>
       <div className="flex justify-between mt-3">
@@ -31,14 +17,17 @@ function CredentialsEdit({
         </div>
       </div>
       <hr className="mb-6"></hr>
-      <label htmlFor="cred_name">Credential Name</label>
+      <label htmlFor="name">Credential Name</label>
       <br />
       <input
         type="text"
         id="cred_name"
         name="cred_name"
-        value={tempData[selectedCredential].cred_name}
-        onChange={handleChange}
+        placeholder="Credential Name"
+        value={
+          tempData[selectedCredential] ? tempData[selectedCredential].name : ""
+        }
+        onChange={(e) => handleChange(e, ["name"])}
       />
       <br />
       <label htmlFor="issuer_name">Issuer Name</label>
@@ -47,8 +36,13 @@ function CredentialsEdit({
         type="text"
         id="issuer_name"
         name="issuer_name"
-        value={tempData[selectedCredential].issuer_name}
-        onChange={handleChange}
+        placeholder="Issuer Name"
+        value={
+          tempData[selectedCredential]
+            ? tempData[selectedCredential].issuer_name
+            : ""
+        }
+        onChange={(e) => handleChange(e, ["issuer_name"])}
       />
       <br />
       <label> Add Attributes</label>
@@ -57,8 +51,8 @@ function CredentialsEdit({
         tempData={tempData}
         selectedCredential={selectedCredential}
         handleChange={handleChange}
-        handleAttributeRemoval={handleAttributeRemoval}
         addAttribute={addAttribute}
+        removeAttribute={removeAttribute}
       />
     </>
   );

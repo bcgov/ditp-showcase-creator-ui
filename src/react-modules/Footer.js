afbf57e29d41e7a345505f6719391d0d4dbcac38
boadoryan <75857938+boadoryan@users.noreply.github.com>
2023-07-27 15:42:54 -0700

    changed footer color

--- src/react-modules/Footer.js
+++ src/react-modules/Footer.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 py-6 text-white text-center relative bottom-0 left-0 w-full">
+    <footer
+      class="py-6 text-white text-center relative bottom-0 left-0 w-full"
+      style={{ backgroundColor: "#454545" }}
+    >
       <p>
         &copy; {new Date().getFullYear()} British Columbia Digital Identity and
         Trust Program. All rights reserved.

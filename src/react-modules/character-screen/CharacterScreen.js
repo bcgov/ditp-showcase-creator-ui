5c242666fc511c653b7ae001bcc3309557d6ad10
boadoryan <75857938+boadoryan@users.noreply.github.com>
2023-08-14 19:29:21 -0700

    reverted changes. cancel/save functionality gets lost.

--- src/react-modules/character-screen/CharacterScreen.js
+++ src/react-modules/character-screen/CharacterScreen.js
@@ -83,14 +83,26 @@ function CharacterScreen({
 
             {/* <div className="w-1/2 two-column-col  bg-light-bg-secondary dark:bg-dark-bg-secondary  p-3 rounded-md right-col "> */}
             <div className="w-3/5 two-column-col  bg-light-bg-secondary dark:bg-dark-bg-secondary  p-3 rounded-md right-col ">
-              <CharacterEdit
-                selectedCharacter={selectedCharacter}
-                handleLocalUpdate={handleLocalUpdate}
-                handleJSONUpdate={handleJSONUpdate} // Updated this line
-                showcaseJSON={showcaseJSON} // Updated this line
-                localJSON={localJSON} // Added this line
-                setCharacterImages={setCharacterImages}
-              />
+              {editMode ? (
+                // Toggling edit mode
+                <CharacterEdit
+                  selectedCharacter={selectedCharacter}
+                  handleLocalUpdate={handleLocalUpdate}
+                  handleJSONUpdate={handleJSONUpdate} // Updated this line
+                  showcaseJSON={showcaseJSON} // Updated this line
+                  localJSON={localJSON} // Added this line
+                  setCharacterImages={setCharacterImages}
+                />
+              ) : (
+                <CharacterInfo
+                  setShowcaseJSON={setShowcaseJSON}
+                  showcaseJSON={showcaseJSON}
+                  setSelectedCharacter={setSelectedCharacter}
+                  selectedCharacter={selectedCharacter}
+                  setEditMode={setEditMode}
+                  characterImages={characterImages}
+                />
+              )}
             </div>
           </div>
           {

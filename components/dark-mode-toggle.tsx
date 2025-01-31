f34782038341fb3955bb3cf5f8d6c441fb4ea491
Ronaldo Lima <ronal2do@gmail.com>
2025-01-31 14:43:09 +0100

    feat: add skeleton loading state for dark mode toggle

--- components/dark-mode-toggle.tsx
+++ components/dark-mode-toggle.tsx
@@ -3,19 +3,20 @@
 import { useMounted } from "@/hooks/use-mounted";
 import { Sun, Moon } from "lucide-react";
 import { useTheme } from "next-themes";
+import { Skeleton } from "./ui/skeleton";
 
 export const DarkModeToggle = () => {
   const { theme, setTheme } = useTheme();
   const isMounted = useMounted();
-
-  if (!isMounted) return null;
   
   const toggleDarkMode = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
 
+  if (!isMounted) return <Skeleton className="border-2 w-16 h-9 bg-transparent rounded-full"/>;
+
   return (
-    <button onClick={toggleDarkMode} className="w-12">
+    <button onClick={toggleDarkMode}>
       <div className="rounded-full p-1 transition-all border-2 w-16 bg-light-bg dark:bg-dark-bg">
         {theme === "dark" ? (
           <Sun className="w-6 h-6 text-yellow-300 ml-6"/>
